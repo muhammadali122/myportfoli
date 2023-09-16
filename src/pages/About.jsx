06e306519d7c20c648e12e5aa183de31381439c5
Muhammad-Ali <muhammadali0030@yahoo.com>
2023-09-16 13:31:39 +0500

    Update home and about UI

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import profilePic from "../assets/images/profile-pic.png";
+import { Link } from "react-router-dom";
+import myCv from "../assets/files/Muhammad Ali React Js.pdf"
 
 function About() {
   return (
@@ -15,27 +17,56 @@ function About() {
           </h1>
           <div>
             <Row className="pt-4">
-              <Col sm={3} className="fw-bold">Name</Col>
-              <Col sm={9} className="text-secondary opacity-100">Muhammad Ali</Col>
+              <Col sm={3} className="fw-bold">
+                Name
+              </Col>
+              <Col sm={9} className="text-secondary opacity-100">
+                Muhammad Ali
+              </Col>
             </Row>
             <Row className="pt-2">
-              <Col sm={3} className="fw-bold">Date Of Birth</Col>
-              <Col sm={9} className="text-secondary opacity-100">March 20, 2001</Col>
+              <Col sm={3} className="fw-bold">
+                Date Of Birth
+              </Col>
+              <Col sm={9} className="text-secondary opacity-100">
+                March 20, 2001
+              </Col>
             </Row>
             <Row className="pt-2">
-              <Col sm={3} className="fw-bold">Address</Col>
-              <Col sm={9} className="text-secondary opacity-100">Punjab Lahore</Col>
+              <Col sm={3} className="fw-bold">
+                Address
+              </Col>
+              <Col sm={9} className="text-secondary opacity-100">
+                Punjab Lahore
+              </Col>
             </Row>
             <Row className="pt-2">
-              <Col sm={3} className="fw-bold">Email</Col>
-              <Col sm={9} className="text-secondary opacity-100">muhammadali0030@yahoo.com</Col>
+              <Col sm={3} className="fw-bold">
+                Email
+              </Col>
+              <Col sm={9} className="text-secondary opacity-100">
+                muhammadali0030@yahoo.com
+              </Col>
             </Row>
             <Row className="pt-2">
-              <Col sm={3} className="fw-bold">Phone</Col>
-              <Col sm={9} className="text-secondary opacity-100">+923489310772</Col>
+              <Col sm={3} className="fw-bold">
+                Phone
+              </Col>
+              <Col sm={9} className="text-secondary opacity-100">
+                +923489310772
+              </Col>
             </Row>
             <div className="mt-4">
-              <Button variant="warning fw-500 rounded-pill">DOWNLOAD CV</Button>
+              <Button variant="warning fw-500 rounded-pill">
+                <Link
+                  className="text-decoration-none text-dark bg-transparent hire-me-link"
+                  to={myCv}
+                  download="Muhammad Ali Front-End Develper.pdf"
+                  target="_blank"
+                >
+                  DOWNLOAD CV
+                </Link>
+              </Button>
             </div>
           </div>
         </Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,48 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Button, Container, Image } from "react-bootstrap";
+
 import profilePic from "../assets/images/profile-pic.png";
+import About from "./About";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <Container
-    
-        className="d-flex flex-md-row flex-column align-items-center"
-      >
+      <Container className="d-flex flex-md-row flex-column align-items-center">
         <Container className="text-center mb-md-none mb-3">
-          <Image src={profilePic} className="img-responsive" fluid rounded/>
+          <Image src={profilePic} className="img-responsive" fluid rounded />
         </Container>
         <Container className="me-auto text-light">
           <h6 className="text-warning">HELLO!</h6>
           <h1 className="fw-bolder text-md-start text-center fw-100">
             I'm <span className="text-warning ">Muhammad Ali</span>
           </h1>
-          <h3 className=" text-md-start text-center">A Front-End Developer based in Lahore</h3>
+          <h3 className=" text-md-start text-center">
+            A Front-End Developer based in Lahore
+          </h3>
           <div className="d-flex mt-3 text-md-start justify-content-md-start justify-content-center">
-            <Button className="rounded-pill bg-warning text-dark border-0 me-2 home-btns fw-500">HIRE ME</Button>
-            <Button variant="outline-light"  className="rounded-pill text-light home-btns fw-500 border-">MY WORK</Button>
+            <Button className="rounded-pill bg-warning text-dark border-0 me-2 home-btns fw-500">
+              <Link
+                className="text-decoration-none text-dark bg-transparent hire-me-link"
+                to="https://www.upwork.com"
+                target="_blank"
+              >
+                HIRE ME
+              </Link>
+            </Button>
+            <Button
+              variant="outline-light"
+              className="rounded-pill text-light home-btns fw-500 border-"
+            >
+              <Link
+                className="text-decoration-none text-light bg-transparent my-work-link"
+                to="https://www.upwork.com"
+                target="_blank"
+              >
+                MY WORK
+              </Link>
+            </Button>
           </div>
         </Container>
       </Container>
