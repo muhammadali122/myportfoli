75a660a99ad22b4ac638dc0ec6ee04c10f82d8ae
Muhammad-Ali <muhammadali0030@yahoo.com>
2023-09-16 11:02:25 +0500

    fixed route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Header from "./layout/Header";
 import Home from "./pages/Home";
@@ -13,10 +8,9 @@ import About from "./pages/About";
 const App = () => {
   return (
     <>
-      <BrowserRouter >
+      <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
